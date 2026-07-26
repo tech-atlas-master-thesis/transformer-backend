a590789ba1e17f45da824a2d1e42af9a9b0ebb75
Fabian Mooslechner <fabian@mooslechner.dev>
2026-07-26 21:46:20 +0200

    Add time selection to projects

--- pipeline_configs/transform_steps/project_extract.py
+++ pipeline_configs/transform_steps/project_extract.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 from typing import Optional, Union, List, Dict, Any
 
@@ -23,6 +24,27 @@ class ProjectExtractStep(StepConfig):
             raise FileNotFoundError("No scraper data found")
         yield "Data found", EventType.INFO
 
+        START_DATE_FROM = (
+            pd.to_datetime(user_config.get("START_DATE_FROM"), format="ISO8601")
+            if user_config.get("START_DATE_FROM")
+            else None
+        )
+        START_DATE_UNTIL = (
+            pd.to_datetime(user_config.get("START_DATE_UNTIL"), format="ISO8601")
+            if user_config.get("START_DATE_UNTIL")
+            else None
+        )
+        END_DATE_FROM = (
+            pd.to_datetime(user_config.get("END_DATE_FROM"), format="ISO8601")
+            if user_config.get("END_DATE_FROM")
+            else None
+        )
+        END_DATE_UNTIL = (
+            pd.to_datetime(user_config.get("END_DATE_UNTIL"), format="ISO8601")
+            if user_config.get("END_DATE_UNTIL")
+            else None
+        )
+
         RELEVANT_COLUMNS = [
             "externalId",
             "uri",
@@ -44,11 +66,82 @@ class ProjectExtractStep(StepConfig):
             if column not in SCRAPER_DATA.columns:
                 SCRAPER_DATA[column] = None
 
+        # TODO: convert this in the scraper pipeline
+        SCRAPER_DATA["start"] = self._convert_to_date(SCRAPER_DATA["start"])
+        SCRAPER_DATA["end"] = self._convert_to_date(SCRAPER_DATA["end"])
         projects = SCRAPER_DATA[RELEVANT_COLUMNS].to_dict("records")
-        yield projects, EventType.RESULT
+        yield [
+            project
+            for project in projects
+            if self._is_in_date_range(project, START_DATE_FROM, START_DATE_UNTIL, END_DATE_FROM, END_DATE_UNTIL)
+        ], EventType.RESULT
+
+    def _convert_to_date(self, series: pd.Series) -> pd.Series:
+        return pd.to_datetime(series, format="mixed", utc=True).replace({pd.NaT: None})
+
+    def _is_in_date_range(
+        self,
+        project: Dict[str, Any],
+        start_date_from: datetime.datetime,
+        start_date_until: datetime.datetime,
+        end_date_from: datetime.datetime,
+        end_date_until: datetime.datetime,
+    ) -> bool:
+        if (start_date_from or start_date_until) and ("start" not in project or not project["start"]):
+            return False
+        if (end_date_from or end_date_until) and ("end" not in project or not project["end"]):
+            return False
+
+        if start_date_from and project["start"] < start_date_from:
+            return False
+        if start_date_until and project["start"] > start_date_until:
+            return False
+        if end_date_from and project["end"] < end_date_from:
+            return False
+        if end_date_until and project["end"] > end_date_until:
+            return False
+
+        return True
 
     def user_config(self) -> List[StepUserConfig]:
-        return []
+        return [
+            StepUserConfig(
+                "START_DATE_FROM",
+                LocalisationString("Project Start Date From", "Projektbeginn von"),
+                None,
+                StepUserConfig.StepUserConfigType.DATE,
+                None,
+                required=False,
+                format="dd.mm.yy",
+            ),
+            StepUserConfig(
+                "START_DATE_UNTIL",
+                LocalisationString("Project Start Date Until", "Projektstart bis"),
+                None,
+                StepUserConfig.StepUserConfigType.DATE,
+                None,
+                required=False,
+                format="dd.mm.yy",
+            ),
+            StepUserConfig(
+                "END_DATE_FROM",
+                LocalisationString("Project End Date From", "Projektende von"),
+                None,
+                StepUserConfig.StepUserConfigType.DATE,
+                None,
+                required=False,
+                format="dd.mm.yy",
+            ),
+            StepUserConfig(
+                "END_DATE_UNTIL",
+                LocalisationString("Project End Date Until", "Projektende bis"),
+                None,
+                StepUserConfig.StepUserConfigType.DATE,
+                None,
+                required=False,
+                format="dd.mm.yy",
+            ),
+        ]
 
     @staticmethod
     def name() -> str:
